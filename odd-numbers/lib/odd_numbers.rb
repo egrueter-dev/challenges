25870d44cc05ffc57b1b8e85a2ee1baf623d5862
Erik Grueter <erikgrueter@gmail.com>
2015-01-02 12:27:34 +0100

    Second challenge completed

--- odd-numbers/lib/odd_numbers.rb
+++ odd-numbers/lib/odd_numbers.rb
@@ -1,1 +1,9 @@
-# YOUR CODE HERE
+
+i = 0
+
+while i < 100
+  if i % 2 != 0
+    puts i
+  end
+    i += 1
+end
--- rightmost-occurrence/lib/rightmost_occurrence.rb
+++ rightmost-occurrence/lib/rightmost_occurrence.rb
@@ -1,1 +1,16 @@
-# YOUR CODE HERE
+# # # YOUR CODE HERE
+
+def right_most( word, c )
+  count = 1
+  split = word.split('')
+  split.reverse!
+  split.each do |letter|
+    count -= 1
+    if letter == c
+      puts count
+      break
+    end
+  end
+end
+
+puts right_most('erikthekool','k')
