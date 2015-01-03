712c0d38e0d079ec6f371d753275b445bca4b37d
Erik Grueter <erikgrueter@gmail.com>
2015-01-03 19:33:01 +0100

    adding calendar challenge, complete

--- leap-year/lib/leap_year.rb
+++ leap-year/lib/leap_year.rb
@@ -1,3 +1,16 @@
-def leap_year?(year)
-  # YOUR CODE HERE
+def leap_year(year)
+  if year % 4 == 0
+    puts "#{year} is not a leap year"
+
+  elsif year % 100 != 0
+    puts "#{year} is a leap year"
+
+  elsif year % 400 != 0
+    puts "#{year} is not a leap year"
+
+  else
+    puts "#{year} is a leap year"
+  end
 end
+
+puts leap_year(1923)
