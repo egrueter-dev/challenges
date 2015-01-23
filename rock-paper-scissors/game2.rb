dd45735203a0732dc8d9d7027f99361156842d32
Erik Grueter <erikgrueter@gmail.com>
2015-01-23 16:05:39 +0100

    final updates to the rock, paper scissors game

--- rock-paper-scissors/game2.rb
+++ rock-paper-scissors/game2.rb
@@ -27,33 +27,32 @@ p "......Player, please select either Rock(r), Paper(p) or Scissors!(s)"
 computer_selection = array[rand(0..length -1)]
 player_selection = gets.chomp
 
-  if player_selection !='p' || player_selection !='s' || player_selection !='r'
-    p "Please choose from either Paper (p) Rock (r) or Scissor (s)"
-    p "Round restarting....."
+  if (player_selection == 'p' && computer_selection == 's')
+    p "Computer Wins with Scissors"
+    computer += 1
+  elsif (player_selection == 'p' && computer_selection == 'r')
+    p "Player Wins with Paper"
+    player += 1
+  elsif (player_selection == 's' && computer_selection == 'p')
+    p "Player Wins with Scissors"
+    player += 1
+  elsif (player_selection == 's' && computer_selection == 'r')
+    p "Computer Wins with Rock"
+    computer += 1
+  elsif (player_selection == 'r' && computer_selection == 's')
+    p "Player Wins with Rock"
+    player += 1
+  elsif (player_selection == 's' && computer_selection == 'p')
+    p "Computer Wins with Paper"
+    computer += 1
+  elsif (player_selection == computer_selection)
+    p "Tie!"
+    p "You both chose: #{computer_selection}"
+  else
+       p "Please choose from either Paper (p) Rock (r) or Scissor (s)"
+       p "Round restarting....."
   end
-    if (player_selection == 'p' && computer_selection == 's')
-      p "Computer Wins with Scissors"
-      computer += 1
-    elsif (player_selection == 'p' && computer_selection == 'r')
-      p "Player Wins with Paper"
-      player += 1
-    elsif (player_selection == 's' && computer_selection == 'p')
-      p "Player Wins with Scissors"
-      player += 1
-    elsif (player_selection == 's' && computer_selection == 'r')
-      p "Computer Wins with Rock"
-      computer += 1
-      elsif (player_selection == 'r' && computer_selection == 's')
-      p "Player Wins with Rock"
-      player += 1
-    elsif (player_selection == 's' && computer_selection == 'p')
-      p "Computer Wins with Paper"
-      computer += 1
-    elsif (player_selection == computer_selection)
-      p "Tie!"
-      p "You both chose: #{computer_selection}"
-    end
-  round += 1
+round += 1
 end
 
 puts "Final Scores:"
