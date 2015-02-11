da5200a5b34af826c49ad7531ffa14fcbf7cb2de
Erik Grueter <erikgrueter@gmail.com>
2015-02-11 17:28:57 -0500

    updating challenge work

---
+++ optimal-guesser/guess_number.rb
@@ -0,0 +1,27 @@
+# def guess_number(min, max)
+#   # You can call the `check` method with a number to see if it
+#   # is the hidden value.
+#   #
+#   # If the guess is correct, it will return 0.
+#   # If the guess is too high, it will return 1.
+#   # If the guess is too low, it will return -1.
+#   #
+#   # If you call `check` too many times, the program will crash.
+#   #
+#   # e.g. if the hidden number is 43592, then
+#   #
+#   # check(50000) # => 1
+#   # check(40000) # => -1
+#   # check(43592) # => 0
+#   #
+#   # When you've figured out what the hidden number is, return it
+#   # from this method.
+#
+#
+# end
+min = 1
+max = 100
+hidden_number = 3
+
+guess = rand(min..max)
+puts (min...max).sample(1)
--- pig-latin-translator/translate.rb
+++ pig-latin-translator/translate.rb
@@ -1,6 +1,36 @@
 def translate(sentence)
-  # YOUR CODE GOES HERE
+
+  # * If the word begins with a consonant or consonant
+  # cluster, move the consonant(s) to the end of the word and append *ay*
+  # (e.g. **happy** becomes **appyhay**, **child** becomes **ildchay**).
+
+  # * If the word begins with a vowel
+  # append *way* to the end of the word
+  # (e.g. **egg** becomes **eggway**)
+
+ #
+ # vowels = ("a","e","i","o","u")
+ # consonant = "all else"
+ #
+
+sentence = sentence.split(" ")
+
+sentence.each do |word|
+  if word.start_with?("a","e","i","o","u") == true
+    word << "way"
+  elsif word.start_with?("a","e","i","o","u") == false
+    first_l = word.slice!(0)
+    append = first_l + "ay"
+    word << append
+  end
+end
+
+print sentence
+
+
+
+
 end
 
 input = ARGV.join(" ")
-puts translate(input)
+puts translate("happy days")
