8ce46afb267136887e9b70a9d4ab4650fcc1190d
Erik Grueter <erikgrueter@gmail.com>
2015-02-12 10:52:59 -0500

    all tests pass

--- pig-latin-translator/translate.rb
+++ pig-latin-translator/translate.rb
@@ -1,36 +1,29 @@
 def translate(sentence)
-
-  # * If the word begins with a consonant or consonant
-  # cluster, move the consonant(s) to the end of the word and append *ay*
-  # (e.g. **happy** becomes **appyhay**, **child** becomes **ildchay**).
-
-  # * If the word begins with a vowel
-  # append *way* to the end of the word
-  # (e.g. **egg** becomes **eggway**)
-
- #
- # vowels = ("a","e","i","o","u")
- # consonant = "all else"
- #
+#
+#   # * If the word begins with a consonant or consonant
+#   # cluster, move the consonant(s) to the end of the word and append *ay*
+#   # (e.g. **happy** becomes **appyhay**, **child** becomes **ildchay**).
+#
+#   # * If the word begins with a vowel
+#   # append *way* to the end of the word
+#   # (e.g. **egg** becomes **eggway**)
+#
 
 sentence = sentence.split(" ")
-
-sentence.each do |word|
-  if word.start_with?("a","e","i","o","u") == true
-    word << "way"
-  elsif word.start_with?("a","e","i","o","u") == false
-    first_l = word.slice!(0)
-    append = first_l + "ay"
-    word << append
+  sentence.each do |word|
+    if word.start_with?("a","e","i","o","u") == true
+      word << "way"
+    elsif word.start_with?("a","e","i","o","u") == false
+       con_chunk = word.scan(/[^aeiou]+/)[0]
+       word.slice!(con_chunk)
+       append = con_chunk + "ay"
+       word << append
+    end
   end
-end
 
-print sentence.join(" ")
-
-
-
+  return sentence.join(" ").chomp
 
 end
 
-input = ARGV.join(" ")
-puts translate("pig latin translator")
+ input = ARGV.join(" ")
+ translate("child daisy joe")
