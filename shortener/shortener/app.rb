0c5d9bd7b6b475ab090cf2c5330e1e9529148762
Erik <Grueter@Eriks-MacBook-Pro.local>
2015-02-28 19:06:21 -0500

    refactored working shortener

--- shortener/shortener/app.rb
+++ shortener/shortener/app.rb
@@ -30,23 +30,30 @@ def shortened_url(string)
   short_url = ("http://www.website/" + string)
 end
 
-def select_from_db(select_url,db_name,long_eql)
-  "SELECT #{{select_url}} from #{{db_name}} WHERE long='#{long_eql}';"
+def create_redirect_url(url_final)
+  redirect_url = "http://" + "#{url_final.to_a[0]["long"]}"
+end
+
+def insert_urls(long_url,short_url)
+  server_connect do |conn|
+    conn.exec_params("INSERT INTO urls (long,short) VALUES ('#{long_url}','#{short_url}');")
+  end
+end
+
+def select_url(short_or_long,long_or_short,ref_url)
+  "SELECT #{short_or_long} from urls WHERE #{long_or_short}='#{ref_url}';"
 end
 
 get '/' do
   @title = "Welcome to the Link Shortener!"
-  short = nil
+  short = []
   long = params.to_a
-
   if !long.empty?
     long_url = long[0][0]
-      server_connect do |conn|
-
-        short = conn.exec("SELECT short from urls WHERE long='#{long_url}';")
-
-      end
-      final_short = short.first["short"]
+    server_connect do |conn|
+      short = conn.exec(select_url("short","long",long_url))
+    end
+  final_short = short.first["short"]
   end
   erb :index, locals: { short_url: final_short, long_url: long_url }
 end
@@ -54,17 +61,15 @@ end
 post "/" do
   long_url = params[:url]
   short_url = shortened_url(random_string)
-  server_connect do |conn|
-    conn.exec_params("INSERT INTO urls (long,short) VALUES ('#{long_url}','#{short_url}');")
-  end
+  insert_urls(long_url,short_url)
   redirect("/?#{long_url}")
 end
 
 get "/:url" do
  url_final = ''
-  server_connect do |conn|
-   url_final = conn.exec("SELECT long FROM urls WHERE short = '/#{params[:url]}';")
+ server_connect do |conn|
+   url_final = conn.exec(select_url("long","short",params[:url]))
  end
-   redirect_url = "http://" + "#{url_final.to_a[0]["long"]}"
+   redirect_url = create_redirect_url(url_final)
    redirect("#{redirect_url}")
 end
--- shortener/shortener/public/javascripts/app.js
+++ shortener/shortener/public/javascripts/app.js
@@ -1,10 +1,8 @@
 //what is the purpose of the open function?
 
 $(function() {
-
   $("#input_form").on('submit', function(event){
     var input = $('#text_field').val();
     console.log(input);
-    
   });
 });
